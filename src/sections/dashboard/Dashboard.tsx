53db39cb9c199c8f2e98af5ae5b9e9c98f749a6e
Alan Benavides <bruno@coastt.org>
2023-05-17 07:51:46 -0400

    add: domain model GitHub

--- src/sections/dashboard/Dashboard.tsx
+++ src/sections/dashboard/Dashboard.tsx
@@ -11,11 +11,11 @@ import { ReactComponent as Start } from "../../assets/svg/star.svg";
 import { ReactComponent as Unlock } from "../../assets/svg/unlock.svg";
 import { ReactComponent as Watchers } from "../../assets/svg/watchers.svg";
 import { config } from "../../devdash_config";
+import { GitHubRepository } from "../../domain/GitHubRepository";
 import { GitHubApiGitHubRepositoryRepository } from "../../infrastructure/GitHubApiGitHubRepositoryRepository";
-import { GitHubApiResponses } from "../../infrastructure/GithubApiResponses";
 import styles from "./Dashboard.module.scss";
 
-const isoToReadableDate = (lastUpdate: string): string => {
+const isoToReadableDate = (lastUpdate: Date): string => {
 	const lastUpdateDate = new Date(lastUpdate);
 	const currentDate = new Date();
 	const diffTime = currentDate.getTime() - lastUpdateDate.getTime();
@@ -34,14 +34,15 @@ const isoToReadableDate = (lastUpdate: string): string => {
 
 export function Dashboard() {
 	const repository = new GitHubApiGitHubRepositoryRepository(config.github_access_token);
-	const [repositoryData, setRepositoryData] = useState<GitHubApiResponses[]>([]);
+	const [repositoryData, setRepositoryData] = useState<GitHubRepository[]>([]);
 
 	useEffect(() => {
 		repository
 			.search(config.widgets.map((widget) => widget.repository_url))
 			.then((repositoryData) => {
 				setRepositoryData(repositoryData);
-			});
+			})
+			.catch(console.error);
 	}, []);
 
 	return (
@@ -52,61 +53,62 @@ export function Dashboard() {
 					<h1 className={styles.app__brand}>DevDash_</h1>
 				</section>
 			</header>
-			<section className={styles.container}>
-				{repositoryData.map((widget) => (
-					<article className={styles.widget} key={widget.repositoryData.id}>
-						<header className={styles.widget__header}>
-							<a
-								className={styles.widget__title}
-								href={widget.repositoryData.html_url}
-								target="_blank"
-								title={`${widget.repositoryData.organization.login}/${widget.repositoryData.name}`}
-								rel="noreferrer"
-							>
-								{widget.repositoryData.organization.login}/{widget.repositoryData.name}
-							</a>
-							{widget.repositoryData.private ? <Lock /> : <Unlock />}
-						</header>
-						<div className={styles.widget__body}>
-							<div className={styles.widget__status}>
-								<p>Last update {isoToReadableDate(widget.repositoryData.updated_at)}</p>
-								{widget.ciStatus.workflow_runs.length > 0 && (
-									<div>
-										{widget.ciStatus.workflow_runs[0].status === "completed" ? (
-											<Check />
-										) : (
-											<Error />
-										)}
-									</div>
-								)}
+			{repositoryData.length === 0 ? (
+				<div className={styles.empty}>
+					<span>No hay widgets configurados.</span>
+				</div>
+			) : (
+				<section className={styles.container}>
+					{repositoryData.map((widget) => (
+						<article className={styles.widget} key={`${widget.id.organization}/${widget.id.name}`}>
+							<header className={styles.widget__header}>
+								<h2 className={styles.widget__title}>
+									<a
+										href={widget.url}
+										target="_blank"
+										title={`${widget.id.organization}/${widget.id.name}`}
+										rel="noreferrer"
+									>
+										{widget.id.organization}/{widget.id.name}
+									</a>
+								</h2>
+								{widget.private ? <Lock /> : <Unlock />}
+							</header>
+							<div className={styles.widget__body}>
+								<div className={styles.widget__status}>
+									<p>Last update {isoToReadableDate(widget.updatedAt)}</p>
+									{widget.hasWorkflows && (
+										<div>{widget.isLastWorkflowSuccess ? <Check /> : <Error />}</div>
+									)}
+								</div>
+								<p className={styles.widget__description}>{widget.description}</p>
 							</div>
-							<p className={styles.widget__description}>{widget.repositoryData.description}</p>
-						</div>
-						<footer className={styles.widget__footer}>
-							<div className={styles.widget__stat}>
-								<Start />
-								<span>{widget.repositoryData.stargazers_count}</span>
-							</div>
-							<div className={styles.widget__stat}>
-								<Watchers />
-								<span>{widget.repositoryData.watchers_count}</span>
-							</div>
-							<div className={styles.widget__stat}>
-								<Forks />
-								<span>{widget.repositoryData.forks_count}</span>
-							</div>
-							<div className={styles.widget__stat}>
-								<IssueOpened />
-								<span>{widget.repositoryData.open_issues_count}</span>
-							</div>
-							<div className={styles.widget__stat}>
-								<PullRequests />
-								<span>{widget.pullRequests.length}</span>
-							</div>
-						</footer>
-					</article>
-				))}
-			</section>
+							<footer className={styles.widget__footer}>
+								<div className={styles.widget__stat}>
+									<Start />
+									<span>{widget.stars}</span>
+								</div>
+								<div className={styles.widget__stat}>
+									<Watchers />
+									<span>{widget.watchers}</span>
+								</div>
+								<div className={styles.widget__stat}>
+									<Forks />
+									<span>{widget.forks}</span>
+								</div>
+								<div className={styles.widget__stat}>
+									<IssueOpened />
+									<span>{widget.issues}</span>
+								</div>
+								<div className={styles.widget__stat}>
+									<PullRequests />
+									<span>{widget.pullRequests}</span>
+								</div>
+							</footer>
+						</article>
+					))}
+				</section>
+			)}
 		</>
 	);
 }
